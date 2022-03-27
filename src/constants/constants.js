affb5002a9b324c442b90bd11fec027e80e88600
codingshogun <kaushiksumralol538@gmail.com>
2022-03-27 06:36:12 +0530

    changed

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "E-commerce",
     description:
       "A full stack e-commerce website created using the mern stack. This app has features like full user authentication and authorization, create update delete product options only for admin, user cart, place orders, stripe payments and review a product.",
-    image: "/images/ecommerce.png",
+    image: "/images/ecommerce.PNG",
     tags: ["Mongo", "Express", "React", "Node"],
     source: "https://ecommere-shop.herokuapp.com/",
     visit: "https://github.com/codingshogun/ecommereproject",
@@ -13,7 +13,7 @@ export const projects = [
     title: "Social media",
     description:
       "A full stack social media app created using the mern stack. This app has all the features a social media should have like full user authentication and authorization, create update delete posts, users can like and comment on others posts, follow unfollow others.",
-    image: "/images/socialmediaPNG.png",
+    image: "/images/socialmediaPNG.PNG",
     tags: ["React", "JavaScript"],
     source: "https://social-web-mern.herokuapp.com/",
     visit: "https://github.com/codingshogun/social-web",
@@ -23,7 +23,7 @@ export const projects = [
     title: "Crypto currency api project",
     description:
       "A react project created using rapid Api. This app can easily give all the info on Crypto currencies including latest news in the crypto market.",
-    image: "/images/crypto.png",
+    image: "/images/crypto.PNG",
     tags: ["React", "WebRTC"],
     source: "https://coinapp.netlify.app/",
     visit: "https://github.com/codingshogun/coin-app",
@@ -33,7 +33,7 @@ export const projects = [
     title: "real estat api project",
     description:
       "A react project created using rapid Api. This app gives all the info about real estate.",
-    image: "/images/realestate.png",
+    image: "/images/realestate.PNG",
     tags: ["React", "ChatEngine", "Firebase"],
     source:
       "https://real-estate-api-project-6isl6iezs-codingshogun.vercel.app/",
